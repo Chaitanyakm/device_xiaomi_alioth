7992b2c1d64eeee8797d56e044421de623e36ac1
Chaitanya <chaitanyaarora3@gmail.com>
2023-07-08 22:43:21 +0530

    Alioth: Update build fingerprints as per regions

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -32,5 +32,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/alioth_global/alioth:13/RKQ1.211001.001/V14.0.4.0.TKHMIXM:user/release-keys
 
