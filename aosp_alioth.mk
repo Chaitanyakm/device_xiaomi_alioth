78c9b1cb7ac90283792c832c8d340f0e28ac6904
Chaitanya <chaitanyaarora3@gmail.com>
2023-07-08 22:42:42 +0530

    Alioth: Pixel Extended Bringup

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,18 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Extended stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_INCLUDE_STOCK_ARCORE := false
+PEX_BUILD_TYPE := OFFICIAL
+PEX_MAINTAINER := CHAITANYA
+
+# We are a phone
+IS_PHONE := true
+
+# Gapps Architecture
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+# Boot Aniamation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.6.0.SKHMIXM:user/release-keys
